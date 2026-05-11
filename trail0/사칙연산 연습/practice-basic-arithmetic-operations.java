d2ea9501453256ed270352813dfcd5bfe3c13b67
Ryu JungKyun <164990532+ryujungkyun@users.noreply.github.com>
2026-05-11 14:29:46 +0900

    [Passed] 사칙연산 연습 (91ms, 9MB)

---
+++ trail0/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        int a = 5;
+        int d = 30;
+        int c = a + d;
+        System.out.println(c / 8);
+        System.out.println(c % 8);
+
+    }
+}
