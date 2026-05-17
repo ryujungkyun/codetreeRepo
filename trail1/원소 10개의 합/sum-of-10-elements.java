4c2f2a8c978d36fb1ca7d262437b9c2635f00b7a
Ryu JungKyun <164990532+ryujungkyun@users.noreply.github.com>
2026-05-17 16:45:35 +0900

    [Passed] 원소 10개의 합 (129ms, 10MB)

---
+++ trail1/원소 10개의 합/sum-of-10-elements.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+
+        int[] arr = new int[10];
+        int sum = 0;
+        for(int i = 0; i < 10; i++){
+           arr[i] = sc.nextInt();
+           sum += arr[i];
+        }
+
+        System.out.println(sum);
+    
+
+    }
+}
